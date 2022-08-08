7312e53a8d5b502b6ad5ff268b9e770e33d79838
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2022-08-09 01:48:30 +0200

    WIP

--- src/core/domain/src/main/java/be/koder/library/domain/book/Book.java
+++ src/core/domain/src/main/java/be/koder/library/domain/book/Book.java
@@ -17,15 +17,15 @@ public final class Book {
         this.author = author;
     }
 
+    public BookSnapshot takeSnapshot() {
+        return new BookSnapshot(id, title, isbn, author);
+    }
+
     public static Book createNew(String title, Isbn isbn, String author) {
         return new Book(BookId.createNew(), title, isbn, author);
     }
 
     public static Book fromSnapshot(BookSnapshot snapshot) {
         return new Book(snapshot.id(), snapshot.title(), snapshot.isbn(), snapshot.author());
-    }
-
-    public BookSnapshot takeSnapshot() {
-        return new BookSnapshot(id, title, isbn, author);
     }
 }
