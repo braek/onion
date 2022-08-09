f5b6cd1a1815214b7eea61c719b6cd96f66b37ef
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2022-08-09 13:41:44 +0200

    WIP

--- src/frontend-adapters/acceptance-tests/src/test/java/be/koder/library/test/ListBooksTest.java
+++ src/frontend-adapters/acceptance-tests/src/test/java/be/koder/library/test/ListBooksTest.java
@@ -3,6 +3,7 @@ package be.koder.library.test;
 import be.koder.library.api.book.BookListItem;
 import be.koder.library.test.data.BookObjectMother;
 import be.koder.library.test.presenter.MockAddBookPresenter;
+import be.koder.library.vocabulary.book.BookId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -21,6 +22,7 @@ class ListBooksTest {
     class TestHappyFlow {
 
         private final List<BookListItem> items = new ArrayList<>();
+        private BookId bookId;
 
         @BeforeEach
         void setup() {
@@ -28,6 +30,7 @@ class ListBooksTest {
             var presenter = new MockAddBookPresenter();
             TestApplicationContext.INSTANCE.clear();
             TestApplicationContext.INSTANCE.addBook.addBook(book.title(), book.isbn().toString(), book.author(), presenter);
+            bookId = presenter.getBookId();
             items.addAll(TestApplicationContext.INSTANCE.listBooks.listBooks());
         }
 
@@ -35,6 +38,7 @@ class ListBooksTest {
         @DisplayName("it should return items")
         void itemsReturned() {
             assertThat(items).hasSize(1);
+            assertThat(items.get(0).id()).isEqualTo(bookId);
         }
     }
 }
--- src/test-doubles/src/main/java/be/koder/library/test/presenter/MockAddBookPresenter.java
+++ src/test-doubles/src/main/java/be/koder/library/test/presenter/MockAddBookPresenter.java
@@ -5,13 +5,19 @@ import be.koder.library.vocabulary.book.BookId;
 
 public final class MockAddBookPresenter implements AddBookPresenter {
 
+    private BookId bookId;
+
     @Override
     public void added(BookId bookId) {
-
+        this.bookId = bookId;
     }
 
     @Override
     public void invalidIsbn() {
 
+    }
+
+    public BookId getBookId() {
+        return bookId;
     }
 }
