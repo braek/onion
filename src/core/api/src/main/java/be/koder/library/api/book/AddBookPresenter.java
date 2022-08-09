f3d7f7cfe8e8b3246177ae22886585c073abd624
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2022-08-09 21:28:16 +0200

    WIP

--- src/core/api/src/main/java/be/koder/library/api/book/AddBookPresenter.java
+++ src/core/api/src/main/java/be/koder/library/api/book/AddBookPresenter.java
@@ -3,7 +3,6 @@ package be.koder.library.api.book;
 import be.koder.library.vocabulary.book.BookId;
 
 public interface AddBookPresenter {
-
     void added(BookId bookId);
 
     void invalidIsbn();
